6990f394e16a677b97301840d554d7a4689971ca
Yutax23 <junaydx23@yandex.com>
2024-08-31 08:26:33 +0800

    Update unsendReact.js

--- handle/unsendReact.js
+++ handle/unsendReact.js
@@ -1,6 +1,6 @@
 async function unsendReact(api, event) {
   if (event.type == "message_reaction") {
-    if (event.senderID == api.getCurrentUserID() && event.reaction == "👍") {
+    if (event.senderID == api.getCurrentUserID() && event.reaction == "😠") {
       return api.unsendMessage(event.messageID);
     }
   }
